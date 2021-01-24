7ca215211841e997cb264553aa51d1d601bfd4b6
annel <anne.leacy@dkit.ie>
2021-01-24 18:52:32 +0000

    Code taken from Book: "Big Java Late Objects" Section 9.6: Interface Types page 449

---
+++ src/main/java/dkit/sd2/Measurable.java
@@ -0,0 +1,14 @@
+package dkit.sd2;
+
+/**
+   Describes any class whose objects can be measured.
+*/
+public interface Measurable
+{
+   /**
+      Computes the measure of the object.
+      @return the measure
+   */
+   double getMeasure();
+  
+}
